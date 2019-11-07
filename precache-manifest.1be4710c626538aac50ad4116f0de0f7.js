c1824f73044976895458a9c05aa7ef0dd6a57404
Dextrous <ssjdexucyche@gmail.com>
2019-11-07 16:32:30 +0530

    Updates

---
+++ precache-manifest.1be4710c626538aac50ad4116f0de0f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c603321eb0bae9bb6a",
+    "url": "/lightsoutgame/static/css/main.9332ec0b.chunk.css"
+  },
+  {
+    "revision": "14c603321eb0bae9bb6a",
+    "url": "/lightsoutgame/static/js/main.14c60332.chunk.js"
+  },
+  {
+    "revision": "7baa81f8b666b7d933e5",
+    "url": "/lightsoutgame/static/js/1.7baa81f8.chunk.js"
+  },
+  {
+    "revision": "ee290b2df128de53f2f6",
+    "url": "/lightsoutgame/static/js/runtime~main.ee290b2d.js"
+  },
+  {
+    "revision": "5afddb9f1f5377fd9b66b06bac9f19e0",
+    "url": "/lightsoutgame/index.html"
+  }
+];
